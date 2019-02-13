36756468d288601f84a1685827048f70336aa856
ZonaClark <zona.clark56@gmail.com>
2019-02-13 19:54:10 +0800

    conditional render admin page

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,19 +4,24 @@ import { Link } from 'react-router-dom';
 
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
+import * as ROLES from '../../constants/roles';
 import { AuthUserContext } from '../Session';
 
 const Navigation = () => (
     <div>
         <AuthUserContext.Consumer>
             {authUser =>
-                authUser ? <NavigationAuth /> : <NavigationNonAuth />
+                authUser ? (
+                    <NavigationAuth authUser={authUser} />
+                ) : (
+                    <NavigationNonAuth />
+                )
             }
         </AuthUserContext.Consumer>
     </div>
 );
 
-const NavigationAuth = () => (
+const NavigationAuth = ({ authUser }) => (
     <ul>
         <li>
             <Link to={ROUTES.LANDING}>Landing</Link>
@@ -27,9 +32,11 @@ const NavigationAuth = () => (
         <li>
             <Link to={ROUTES.ACCOUNT}>Account</Link>
         </li>
-        <li>
-            <Link to={ROUTES.ADMIN}>Admin</Link>
-        </li>
+        {authUser.roles.includes(ROLES.ADMIN) && (
+            <li>
+                <Link to={ROUTES.ADMIN}>Admin</Link>
+            </li>
+        )}
         <li>
             <SignOutButton />
         </li>
