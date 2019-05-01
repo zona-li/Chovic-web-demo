ae0fec4d24a159f3552d76f11002c0da1b0619b4
ZonaClark <hl3fm@virginia.edu>
2019-05-01 18:38:36 -0400

    edit reply date

--- src/components/ThankYou/index.js
+++ src/components/ThankYou/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ThankYou = () => (
     <h3 style={{fontFamily: 'Dosis', textAlign: 'center', paddingTop: '50px'}}>
-        Thank you for submitting the application! You should hear back from us before May 1st, 2019.
+        Thank you for submitting the application! You should hear back from us before June 1st, 2019.
     </h3>
 );
 
