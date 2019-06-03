309f25cadb2d513fbb977bf2e77f9e824e73d7d4
ZonaClark <hl3fm@virginia.edu>
2019-06-02 20:00:13 -0400

    change confirmation message

--- src/components/ThankYou/index.js
+++ src/components/ThankYou/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ThankYou = () => (
     <h3 style={{fontFamily: 'Dosis', textAlign: 'center', paddingTop: '50px'}}>
-        Thank you for submitting the application! You should hear back from us before June 1st, 2019.
+        Thank you for submitting the application! You should hear back from us in one week. If not, feel free to reach out to info@chovic.com.
     </h3>
 );
 
