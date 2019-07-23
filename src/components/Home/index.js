a5d32b473294be655dc50133b4a157d789372e69
ZonaClark <haoyang.zona@gmail.com>
2019-07-23 08:14:07 -0400

    set habit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react';
 import Canvas from './HabitBoard/Canvas';
 import ColorPicker from './HabitBoard/ColorPicker';
 import HabitItem from './HabitBoard/HabitItem';
+import AddHabit from './AddHabit';
 
 const TheBoard = () => {
     const [color, setColor] = useState(0);  // Default color is white
+    const [habit, setHabit] = useState();
+
     return (
-        <div className={'board'}>
-            <ColorPicker currentColor={color} setColor={color => setColor(color)} />
-            <HabitItem habit={"Exercise"} />
-            <Canvas currentColor={color} />
+        <div className={'content'}>
+            <div className={'board'}>
+                <ColorPicker currentColor={color} setColor={color => setColor(color)} />
+                <HabitItem habit={"Exercise"} />
+                <Canvas currentColor={color} />
+            </div>
         </div>
     )
 }
