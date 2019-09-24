9776d9d8b3789f120b843d2ef5ffdd32711f278d
ZonaClark <haoyang.zona@gmail.com>
2019-09-24 10:15:44 +0800

    add month to the board

--- src/components/Home/HabitBoard/DayOfMonth.js
+++ src/components/Home/HabitBoard/DayOfMonth.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
 
 import Pixel from './Pixel';
 
@@ -6,22 +7,40 @@ const DayOfMonth = props => {
     const date = new Date();
     const days = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
     const daysArr = Array(days).fill().map((_, i) => i + 1);
+    const monthMap = {
+        0: 'Jan',
+        1: 'Feb',
+        3: 'Mar',
+        4: 'Apr',
+        5: 'May',
+        6: 'Jun',
+        7: 'Jul',
+        8: 'Aug',
+        9: 'Sep',
+        10: 'Oct',
+        11: 'Nov',
+        12: 'Dec'
+    };
+    const month = monthMap[date.getMonth()+1];
     return (
-        <div className={'habitList'}>
-            {/* This is to make days of the month align with the habit pixel entry */}
-            <div className={'habit'}></div>
-            
-            {daysArr.map((day, index) => {
-                return (
-                    <Pixel
-                        key={index}
-                        day={day}
-                        background='white'
-                        onClick={e => {}}
-                    />
-                )
-            })}
-        </div>
+        <>
+            <Typography variant='h5' className={'month'}>{month}</Typography>
+            <div className={'habitList'}>
+                {/* This is to make days of the month align with the habit pixel entry */}
+                <div className={'habit'}></div>
+                
+                {daysArr.map((day, index) => {
+                    return (
+                        <Pixel
+                            key={index}
+                            day={day}
+                            background='white'
+                            onClick={e => {}}
+                        />
+                    )
+                })}
+            </div>
+        </>
     );
 }
 
