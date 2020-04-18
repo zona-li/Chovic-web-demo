fffef31852158620534c4e4890c2e0159b2612c6
ZonaClark <haoyang.zona@gmail.com>
2020-04-18 18:22:11 -0400

    styling the today indicator

--- src/components/Home/HabitBoard/Pixel.js
+++ src/components/Home/HabitBoard/Pixel.js
@@ -7,7 +7,6 @@ export default (props) => {
     let className = background + ' pixel ' + pixelType;
     let todayDate = '';
     if (day === today && pixelType === 'day-of-month') {
-        className = 'today';
         todayDate = 'todayDate'
     }
     
