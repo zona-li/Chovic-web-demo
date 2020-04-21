c4f9321f117760f61feef4de79310382450bad79
ZonaClark <haoyang.zona@gmail.com>
2020-04-21 11:03:51 -0400

    change categories

--- src/components/Home/AddHabit.js
+++ src/components/Home/AddHabit.js
@@ -8,78 +8,81 @@ import AddIcon from '@material-ui/icons/Add';
 import useForm from './useForm';
 
 const initialState = {
-    habit: '',
-    category: 'career'
+  habit: '',
+  category: 'career',
 };
 
-export default props => {
-    const [values, handleChange] = useForm(initialState);
-    const [showError, setShowError] = useState(false);
-    const classes = useStyles();
+export default (props) => {
+  const [values, handleChange] = useForm(initialState);
+  const [showError, setShowError] = useState(false);
+  const classes = useStyles();
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        if (values.habit) {
-            props.onSubmitNewHabit(values);
-            handleChange(initialState);
-        }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (values.habit) {
+      props.onSubmitNewHabit(values);
+      handleChange(initialState);
     }
+  };
 
-    const handleHabitChange = e => {
-        handleChange(e);
-        if (e.target.value.length > 19) {
-            setShowError(true);
-        } else if (showError) {
-            setShowError(false);
-        }
+  const handleHabitChange = (e) => {
+    handleChange(e);
+    if (e.target.value.length > 19) {
+      setShowError(true);
+    } else if (showError) {
+      setShowError(false);
     }
+  };
 
-    return (
-        <form>
-            <TextField
-                label="Add Habit"
-                className={classes.textField}
-                margin="normal"
-                value={values.habit}
-                name="habit"
-                onChange={handleHabitChange}
-                inputProps={{ maxLength: 20 }}
-                onKeyPress={e => {if (e.key === 'Enter') e.preventDefault()}}
-            />
-            <TextField
-                select
-                label="Category"
-                name="category"
-                className={classes.textField}
-                margin="normal"
-                SelectProps={{
-                    native: true,
-                    MenuProps: {
-                      className: classes.menu,
-                    },
-                }}
-                value={values.category}
-                onChange={handleChange}
-            >
-                <option value="career">Career</option>
-                <option value="finance">Finance</option>
-                <option value="fam">Friends and Family</option>
-                <option value="health">Health</option>
-                <option value="fun">Fun</option>
-                <option value="love">Love</option>
-                <option value="growth">Growth</option>
-                <option value="contribution">Contribution</option>
-            </TextField>
-            <Fab 
-                color="primary" 
-                aria-label="add"
-                size="small"
-                className={classes.fab} 
-                onClick={handleSubmit}
-            >
-                <AddIcon />
-            </Fab>
-            {showError && <FormHelperText error={true} className={classes.leftSpacing}>Try to keep it less than 20 characters.</FormHelperText>}
-        </form>
-    )
-}
+  return (
+    <form>
+      <TextField
+        label="Add Habit"
+        className={classes.textField}
+        margin="normal"
+        value={values.habit}
+        name="habit"
+        onChange={handleHabitChange}
+        inputProps={{ maxLength: 20 }}
+        onKeyPress={(e) => {
+          if (e.key === 'Enter') e.preventDefault();
+        }}
+      />
+      <TextField
+        select
+        label="Category"
+        name="category"
+        className={classes.textField}
+        margin="normal"
+        SelectProps={{
+          native: true,
+          MenuProps: {
+            className: classes.menu,
+          },
+        }}
+        value={values.category}
+        onChange={handleChange}
+      >
+        <option value="career">Career</option>
+        <option value="health">Health</option>
+        <option value="growth">Learning</option>
+        <option value="finance">Personal Finance</option>
+        <option value="fam">Relationships</option>
+      </TextField>
+      <Fab
+        color="primary"
+        aria-label="add"
+        size="small"
+        className={classes.fab}
+        onClick={handleSubmit}
+      >
+        <AddIcon />
+      </Fab>
+      {showError && (
+        <FormHelperText error={true} className={classes.leftSpacing}>
+          Try to keep it less than 20 characters.
+        </FormHelperText>
+      )}
+    </form>
+  );
+};
