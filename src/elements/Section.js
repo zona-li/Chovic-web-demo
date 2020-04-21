8a5a5212b900e6806068a32ae192cc3c7dc5fcc6
ZonaClark <haoyang.zona@gmail.com>
2020-04-21 11:29:43 -0400

    take out scrolling bar on home page

--- src/elements/Section.js
+++ src/elements/Section.js
@@ -1,44 +1,41 @@
 import styled from 'styled-components';
 
 const SectionLeft = styled.section`
-    width: 100%;
-    height: 100vh;
-    overflow: scroll;
-    text-align: left;
-    background: ${props => props.background};
-    background-image: ${props => props.backgroundImage};
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
+  width: 100%;
+  height: 100vh;
+  text-align: left;
+  background: ${(props) => props.background};
+  background-image: ${(props) => props.backgroundImage};
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 
-    @media (min-width: 768px) {
-        float: left;
-        width: 50%;
-    }
+  @media (min-width: 768px) {
+    float: left;
+    width: 50%;
+  }
 `;
 
 const SectionRight = styled.section`
-    color: AliceBlue;
-    width: 100%;
-    height: 100vh;
-    overflow: scroll;
-    text-align: center;
-    background: ${props => props.background};
+  color: AliceBlue;
+  width: 100%;
+  height: 100vh;
+  text-align: center;
+  background: ${(props) => props.background};
 
-    @media (min-width: 768px) {
-        float: right;
-        width: 50%;
-    }
+  @media (min-width: 768px) {
+    float: right;
+    width: 50%;
+  }
 `;
 
 const Section = styled.section`
-    color: AliceBlue;
-    width: 100%;
-    height: 100vh;
-    float: left;
-    overflow: scroll;
-    
-    background: ${props => props.background};
+  color: AliceBlue;
+  width: 100%;
+  height: 100vh;
+  float: left;
+
+  background: ${(props) => props.background};
 `;
 
 export { SectionLeft, SectionRight, Section };
