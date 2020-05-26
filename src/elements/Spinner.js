fccf995fc5d6ce1fb444dd5b04b00902a8abcefc
ZonaClark <haoyang.zona@gmail.com>
2020-05-26 13:15:55 -0400

    spinner

---
+++ src/elements/Spinner.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Spinner.css';
+
+export default () => {
+  return (
+    <div className="loadingio-spinner-spin-c5zi6zpzz3">
+      <div className="ldio-q9xeowmgmyj">
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
